570f95d876f00ad7b5b090c96a5785c9b86bc74a
Romain Loth <romain.loth@particeep.com>
2018-01-15 18:13:08 +0100

    harmonize function names with examples from https://www.particeep.com/en/doc/start

--- query_api.py
+++ query_api.py
@@ -14,35 +14,38 @@ from sys        import argv, stderr
 # ROUTE
 SCHEME = "https:"
 # SRV = "localhost:9100"
-SRV = "api.particeep.com"
+SRV = "test-api.particeep.com"
 VER = "v1"
 
 # AUTH KEY
 KEY = "d6a53e1a-fc8e-4251-9dda-fabbce5f2a2c"
 SEC = "9bb3c122-0272-4bed-a632-19d5d52c7b5e"
 
-def make_sign_token(now, key = KEY, sec = SEC):
-    to_sign = sec + key + now
+def build_authorization_header(api_key, api_secret, date_time):
+    to_sign = api_secret + api_key + date_time
     msg_bytes = bytes(to_sign, 'utf-8')
-    sec_bytes = bytes(sec, 'utf-8')
+    sec_bytes = bytes(api_secret, 'utf-8')
     hasher = hmac.new(sec_bytes, msg_bytes, sha1)
     hex_signature = hasher.hexdigest()
     b64_signature = urlsafe_b64encode(bytes(hex_signature, 'utf-8'))
-    return ':'.join(["PTP",KEY,str(b64_signature, 'UTF-8')])
+    return ':'.join(["PTP",api_key,str(b64_signature, 'UTF-8')])
+
+def build_date_header():
+    return datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
 
 def apiget(route):
     """
     endpoint + query => response json
     """
     url = SCHEME + "//" + SRV + '/' + VER + '/' + route
-    now = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+    date_time = build_date_header()
     warn("url:", url)
     # requests.get
     resp = get(
         url,
         headers={
-            "Authorization": make_sign_token(now),
-            "DateTime": now,
+            "Authorization": build_authorization_header(KEY, SEC, date_time),
+            "DateTime": date_time,
             "Accept": "application/json"
         }
     )
